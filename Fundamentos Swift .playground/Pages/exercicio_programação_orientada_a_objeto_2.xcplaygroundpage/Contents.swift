f65c1b48baf180190426a57249fc48f3b51a25d4
ElMarcelFarias <marcel.leitefarias@gmail.com>
2022-12-07 10:18:08 -0300

    Módulo 10 exericio programação orientada a objetos finalizada

---
+++ Fundamentos Swift .playground/Pages/exercicio_programação_orientada_a_objeto_2.xcplaygroundpage/Contents.swift
@@ -0,0 +1,13 @@
+//: [Previous](@previous)
+
+import Foundation
+
+ 
+//1) qual é a diferençe entre Struct e Class?
+//R: Com a Class podemos utilizar o denit para matar uma classe ou metodo, o que não conseguimos fazer com a Struct e a classe é do tipo referência então se caso você declarar uma váriavel dog com o valor de 'Cachorro' e outra váriavel cat que foi atribuida dog var cat = dog, e caso você mude o valor de cat para outro valor a váriavel dog também ira sofrer a alteração, uma das diferenças entre Class e Struct é que a class é do tipo referência e não do tipo valor, semelhante a Struct. Class aponta para o mesmo campo de memória armazenada. Na class precisamos inicializar já na Struct não precisamos.
+
+//2) qual é a relação entre classe e objeto?
+//R: Você precisa do planejamento para construir uma casa, então um depende do outro
+
+//3) qual a diferença entre classe e objeto?
+//R: A Class é como se fosse o esqueleto, a planta de uma casa nada palpavel ou utilizavél, já o objeto são os itens dentro da casa, palpável e manipulavel. Na Class você pode dizer que aquele objeto TERÁ uma cor, já no objeto você pode dizer QUAL é a cor do objeto
