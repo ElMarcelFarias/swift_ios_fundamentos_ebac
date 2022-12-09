4ec31744cf667b1082f52349eeed416d31718025
ElMarcelFarias <marcel.leitefarias@gmail.com>
2022-12-09 11:53:45 -0300

    delegates e protocols parte 2

--- Fundamentos Swift .playground/Pages/protocols_delegates.xcplaygroundpage/Contents.swift
+++ Fundamentos Swift .playground/Pages/protocols_delegates.xcplaygroundpage/Contents.swift
@@ -8,18 +8,30 @@ enum Direction {
     case right
 }
 
-protocol Vehicle {
+protocol ActionVehicle {
     func accelerate()
     func stop()
+}
+
+protocol Vehicle {
     func turn(_ direction: Direction)
+    var name: String {get set}
+    init(initialVehicle: String)
 }
 
 //var vehicle = Vehicle() não posso fazer isso
 
-class Bike: Vehicle {
+class Bike: ActionVehicle,Vehicle {
     
     var peddling = false
     var brakesApplied = false
+    var name: String
+    var direction: Direction
+    
+    required init(initialVehicle: String) {
+        name = initialVehicle
+        direction = .right
+    }
     
     func accelerate() {
         print("acelerar")
@@ -33,7 +45,17 @@ class Bike: Vehicle {
         peddling = false
     }
     
+    
+    
 }
 
-var bike = Bike()
+extension Bike {
+    func turn(_ direction: Direction) {
+        self.direction = direction
+        print(direction)
+    }
+}
+
+var bike = Bike(initialVehicle: "Caloi")
+bike.turn(.left)
 bike.accelerate()
