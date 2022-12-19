2a66556b9a0cc0a694f02ca1369669f5e75c7877
ElMarcelFarias <marcel.leitefarias@gmail.com>
2022-12-19 08:48:15 -0300

    Módulo 13 - Table View e ARC, Aula Gerenciamento de memória com WEAK finalizada!

--- Fundamentos Swift .playground/Pages/tableView_Arc.xcplaygroundpage/Contents.swift
+++ Fundamentos Swift .playground/Pages/tableView_Arc.xcplaygroundpage/Contents.swift
@@ -2,3 +2,43 @@
 import Foundation
 
 /// Table View e ARC
+
+/// ARC aula inteira sobre WEAK referëncia fraca
+/// sempre quando utilizamos a referencia weak nas variáveis ela deverá ser um optional ?.
+
+
+
+class Recipe {
+    let title: String
+    weak var chef: Chef?
+    
+    init(title: String) {
+        self.title = title
+    }
+    
+    deinit {
+        print("GoodBye  Recipe \(title)")
+    }
+}
+
+class Chef {
+    let name: String
+    var recipes: [Recipe] = []
+    
+    init(name: String) {
+        self.name = name
+    }
+    
+    deinit {
+        print("Goodbye \(name)")
+    }
+}
+
+
+
+do {
+    let recipe = Recipe(title: "Gerenciando Memória")
+    let chef = Chef(name: "Marcel Leite de Farias")
+    recipe.chef = chef
+    chef.recipes.append(recipe)
+}
