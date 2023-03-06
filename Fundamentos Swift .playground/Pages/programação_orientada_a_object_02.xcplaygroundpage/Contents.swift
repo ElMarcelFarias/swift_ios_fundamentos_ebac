62d2cfca3f608a25e164c6474710c4f1d12b4694
ElMarcelFarias <marcel.leitefarias@gmail.com>
2023-03-06 13:40:14 -0300

    Release: deixando projeto em standby para iniciar bootcamp na dio

--- Fundamentos Swift .playground/Pages/programação_orientada_a_object_02.xcplaygroundpage/Contents.swift
+++ Fundamentos Swift .playground/Pages/programação_orientada_a_object_02.xcplaygroundpage/Contents.swift
@@ -216,18 +216,24 @@ let newString = "the old string".replace(target: "old", withString: "new")
 print(newString)
 print(email.lenght)
 
-func contactMe() -> [String: String] {
-    
-    return  [
-        "email": "marcel.leitefarias@gmail.com",
-        "linkedin": "https://www.linkedin.com/in/marcel-leite-de-farias-38b62b220/",
-        "whatssap": "+55 47 99928-0278"
-    ]
-}
+var contactMeU: [String: String] = [
+    "email": "marcel.leitefarias@gmail.com",
+    "linkedin": "https://www.linkedin.com/in/marcel-leite-de-farias-38b62b220/",
+    "whatssap": "+55 47 99928-0278"
+]
 
-var contact = contactMe()
+//func contactMe() -> [String: String] {
+//
+//    return  [
+//        "email": "marcel.leitefarias@gmail.com",
+//        "linkedin": "https://www.linkedin.com/in/marcel-leite-de-farias-38b62b220/",
+//        "whatssap": "+55 47 99928-0278"
+//    ]
+//}
 
-for (key,value) in contact {
+//var contact = contactMe()
+
+for (key,value) in contactMeU {
     print("\(key) => \(value)")
 }
 
